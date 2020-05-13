fd2d27b05aab4c58fad31e66b7199256bdafd8a2
Taoufiq-Rhouas <rhouastaoufiq@gmail.com>
2020-05-13 19:24:11 +0000

    Front-end

--- Second pages/FirstPage/confirmation.php
+++ Second pages/FirstPage/confirmation.php
@@ -161,6 +161,7 @@
         <div class="text-light mt-5 mb-5 text-center small">by : <a class="text-light" target="_blank" href="http://totoprayogo.com">totoprayogo.com</a></div>
     </div>
 
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
