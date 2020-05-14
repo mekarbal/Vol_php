a262b56a330d09b039eb86ae1fbf1ecf27f4fe73
Elmahdi KARBAL <56792446+mekarbal@users.noreply.github.com>
2020-05-14 13:15:16 +0000

    push db

--- Second pages/cr.sql
+++ Second pages/cr.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mer. 13 mai 2020 à 21:13
+-- Généré le : jeu. 14 mai 2020 à 15:14
 -- Version du serveur :  10.4.11-MariaDB
--- Version de PHP : 7.4.5
+-- Version de PHP : 7.2.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,12 +29,12 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `clients` (
   `id_client` int(11) NOT NULL,
-  `nom_client` varchar(40) CHARACTER SET utf8 DEFAULT NULL,
-  `prenom_client` varchar(40) CHARACTER SET utf8 DEFAULT NULL,
-  `phone` int(16) DEFAULT NULL,
-  `email` varchar(40) DEFAULT NULL,
-  `cin` varchar(40) CHARACTER SET utf8 DEFAULT NULL,
-  `n_passport` varchar(40) DEFAULT NULL
+  `nom_client` char(122) CHARACTER SET utf8 DEFAULT NULL,
+  `prenom_client` char(122) CHARACTER SET utf8 DEFAULT NULL,
+  `phone` int(11) DEFAULT NULL,
+  `email` int(11) DEFAULT NULL,
+  `cin` char(11) CHARACTER SET utf8 DEFAULT NULL,
+  `n_passport` varchar(233) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -42,10 +42,12 @@ CREATE TABLE `clients` (
 --
 
 INSERT INTO `clients` (`id_client`, `nom_client`, `prenom_client`, `phone`, `email`, `cin`, `n_passport`) VALUES
-(1, 'Taoufiq', 'Rh', 600000000, 'rh@gmail.com', 'CIN-RH', 'NUM-PASPORT-RH'),
-(2, 'Mehdi', 'KR', 600000011, 'kr@gmail.com', 'CIN-KR', 'NUM-PASPORT-KR'),
-(3, 'Mohameed', 'MO', 600000022, 'mo@gmail.com', 'CIN-MO', 'NUM-PASPORT-MO'),
-(4, 'Yassine', 'YA', 600000222, 'ya@gmail.com', 'CIN-YA', 'NUM-PASPORT-YA');
+(3, 'm', 'K', 21222222, 2222, '2', 'DDDD'),
+(4, 'mahdi', 'karbal', 2147483647, 0, 'HH241170', 'HD2122'),
+(5, 'Taoufiq', 'Rh', 600000000, 0, 'CIN-RH', 'NUM-PASPORT-RH'),
+(6, 'Mehdi', 'KR', 600000011, 0, 'CIN-KR', 'NUM-PASPORT-KR'),
+(7, 'Mohameed', 'MO', 600000022, 0, 'CIN-MO', 'NUM-PASPORT-MO'),
+(8, 'Yassine', 'YA', 600000222, 0, 'CIN-YA', 'NUM-PASPORT-YA');
 
 -- --------------------------------------------------------
 
@@ -57,7 +59,7 @@ CREATE TABLE `reservations` (
   `id_reservation` int(200) NOT NULL,
   `vol_id_vol` int(11) NOT NULL,
   `cli_id_client` int(11) NOT NULL,
-  `date_reservation` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `date_reservation` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -65,14 +67,15 @@ CREATE TABLE `reservations` (
 --
 
 INSERT INTO `reservations` (`id_reservation`, `vol_id_vol`, `cli_id_client`, `date_reservation`) VALUES
-(1, 3, 1, '2020-05-13 18:54:56'),
-(2, 4, 2, '2020-05-13 18:59:56'),
-(3, 1, 2, '2020-05-13 19:00:32'),
-(4, 1, 1, '2020-05-13 19:01:26'),
-(5, 7, 1, '2020-05-13 19:04:51'),
-(6, 7, 2, '2020-05-13 19:05:52'),
-(7, 7, 3, '2020-05-13 19:07:18'),
-(8, 7, 4, '2020-05-13 19:08:36');
+(1, 20, 4, NULL),
+(2, 3, 1, '2020-05-13 18:54:56'),
+(3, 4, 2, '2020-05-13 18:59:56'),
+(4, 1, 2, '2020-05-13 19:00:32'),
+(5, 1, 1, '2020-05-13 19:01:26'),
+(6, 7, 1, '2020-05-13 19:04:51'),
+(7, 7, 2, '2020-05-13 19:05:52'),
+(8, 7, 3, '2020-05-13 19:07:18'),
+(9, 7, 4, '2020-05-13 19:08:36');
 
 -- --------------------------------------------------------
 
@@ -99,13 +102,12 @@ CREATE TABLE `vols` (
 --
 
 INSERT INTO `vols` (`id`, `name`, `price`, `image`, `created`, `pays_depart`, `pays_arrive`, `date_vol`, `hour_vol`, `minute_vol`, `nb_place`) VALUES
-(1, 'Voyage de travel', 20000, 'pngfuel.com.png', '2020-05-13 18:38:48', 'maroc', 'france', '2020-05-22', 12, 12, 58),
-(2, 'Voyage en famille', 5000, 'pngfuel.com.png', '2020-05-13 18:38:48', 'maroc', 'españa', '2020-05-26', 10, 10, 100),
-(3, 'Voyage en famille', 10000, 'pngfuel.com.png', '2020-05-13 18:44:28', 'maroc', 'suisse', '2020-06-06', 11, 11, 39),
-(4, 'Voyage de travel', 1500, 'pngfuel.com.png', '2020-05-13 18:44:28', 'maroc', 'espagne', '2020-05-20', 9, 9, 39),
-(5, 'Voyage en famille', 1600, 'pngfuel.com.png', '2020-05-13 18:49:20', 'maroc', 'kanada', '2020-06-26', 8, 8, 20),
-(6, 'Voyage en famille', 1000, 'pngfuel.com.png', '2020-05-13 18:49:20', 'maroc', 'norwegen', '2020-07-22', 4, 4, 20),
-(7, 'Voyage de travel', 40000, 'pngfuel.com.png', '2020-05-13 19:04:18', 'maroc', 'holanda', '2020-07-28', 8, 8, 0);
+(20, 'weld akrbal', 1111, 'pngfuel.com.png', '2020-05-05 00:00:00', '0', '0', '0000-00-00', 0, 0, 2),
+(24, 'SFSF', 0, 'pngfuel.com.png', '2020-05-08 12:46:12', 'FES', 'FESE', '2020-05-20', 12, 12, 123),
+(25, 'vvvvvvvvv', 121, 'pngfuel.com.png', '2020-05-11 15:57:56', 'paris', 'CASA', '2020-05-26', 12, 12, 120),
+(26, 'DFSECG', 300, 'pngfuel.com.png', '2020-05-11 15:57:56', 'USA', 'MAROC-LAAYOUNE', '2020-05-28', 13, 13, 1222),
+(27, 'vvvvvvvvv', 121, 'pngfuel.com.png', '2020-05-13 12:55:43', 'paris', 'CASA', '2020-05-26', 12, 12, 120),
+(28, 'DFSECG', 300, 'pngfuel.com.png', '2020-05-13 12:55:43', 'USA', 'MAROC-LAAYOUNE', '2020-05-28', 13, 13, 1222);
 
 --
 -- Index pour les tables déchargées
@@ -140,19 +142,19 @@ ALTER TABLE `vols`
 -- AUTO_INCREMENT pour la table `clients`
 --
 ALTER TABLE `clients`
-  MODIFY `id_client` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_client` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT pour la table `reservations`
 --
 ALTER TABLE `reservations`
-  MODIFY `id_reservation` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_reservation` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT pour la table `vols`
 --
 ALTER TABLE `vols`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
